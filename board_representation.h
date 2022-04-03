7fa5955b623efbc5a740b072afe6a7b6a6a40b99
rp6kp <rp6kp@umsystem.edu>
2022-04-03 18:56:55 -0500

    feat: piece, piece list structs

--- board_representation.h
+++ board_representation.h
@@ -4,11 +4,123 @@
 #include <iostream>
 namespace Chess
 {
-    namespace Piece{
 
+    namespace Piece
+    {
+        /**
+         * @brief 5 bits are used to encode the pieces
+         *  Starting the from little endian
+         * -----------------------------
+         * Piece type
+         * -----------------------------
+         * 1 st bit is diagonal moving
+         * 2 nd bit is straight
+         * 3 rd bit is sliding piece
+         * ----------------------------
+         * Color Representation bits
+         * --------------------------
+         * 4 th bit is white
+         * 5 th bit is black
+         *
+         */
+        const int None = 0;
+
+        const int King = 1;
+
+        const int Pawn = 2;
+
+        const int Knight = 3;
+
+        const int Bishop = 5;
+
+        const int Rook = 6;
+
+        const int Queen = 7;
+
+        const int White = 8;
+
+        const int Black = 16;
+
+        const int typeMask = 0b00111;
+
+        const int blackMask = 0b10000;
+
+        const int whiteMask = 0b01000;
+
+        const int colourMask = whiteMask | blackMask;
+
+        bool IsGivenColour(int piece, int colour)
+        {
+            return (piece & colourMask) == colour;
+        }
+
+        int getPieceColour(int piece)
+        {
+            return piece & colourMask;
+        }
+
+        int getPieceType(int piece)
+        {
+            return piece & typeMask;
+        }
+
+        bool isRookOrQueen(int pieceType)
+        {
+            return (pieceType & 0b110) == 0b110;
+        }
+
+        bool isBishopOrQueen(int pieceType)
+        {
+            return (pieceType & 0b101) == 0b101;
+        }
+
+        bool isSlidingPiece(int pieceType)
+        {
+            return (pieceType & 0b100) != 0;
+        }
     }
     namespace BoardRepresentation
     {
+        struct PieceList
+        {
+            int *occupiedSquares;
+            int *map;
+            int numPieces;
+
+            PieceList(int maxPieceCount = 16)
+            {
+                occupiedSquares = new int[maxPieceCount]();
+                map = new int[64]();
+                numPieces = 0;
+            }
+
+            void addPieceAtSquare(int square)
+            {
+                occupiedSquares[numPieces] = square;
+                map[square] = square;
+                numPieces++;
+            }
+
+            void removePieceAtSquare(int square)
+            {
+                int pieceIndex = map[square];
+                occupiedSquares[pieceIndex] = occupiedSquares[numPieces - 1];
+                map[occupiedSquares[pieceIndex]] = pieceIndex;
+                numPieces--;
+            }
+
+            void MovePiece(int startSquare, int targetSquare)
+            {
+                int pieceIndex = map[startSquare];
+                occupiedSquares[pieceIndex] = targetSquare;
+                map[targetSquare] = pieceIndex;
+            }
+
+            const int operator[](int index) const
+            {
+                return occupiedSquares[index];
+            }
+        };
         struct Coord
         {
             int fileIndex;
@@ -32,7 +144,7 @@ namespace Chess
         {
             os << coord.fileIndex << " " << coord.rankIndex;
         }
-        const std::string fileNames = std::string("abcedfgh");
+        const std::string fileNames = std::string("abcdefgh");
         const std::string rankNames = std::string("12345678");
         const int a1 = 0;
         const int b1 = 1;
@@ -52,42 +164,42 @@ namespace Chess
         const int g8 = 62;
         const int h8 = 63;
 
-         int getRankIndex(int squareIndex)
+        int getRankIndex(int squareIndex)
         {
             return squareIndex >> 3;
         }
 
-         int getFileIndex(int squareIndex)
+        int getFileIndex(int squareIndex)
         {
             return squareIndex & 0b000111;
         }
 
-         int IndexFromCoord(int fileIndex, int rankIndex)
+        int IndexFromCoord(int fileIndex, int rankIndex)
         {
             return rankIndex * 8 + fileIndex;
         }
 
-         int IndexFromCoord(Coord coord)
+        int IndexFromCoord(Coord coord)
         {
             return IndexFromCoord(coord.fileIndex, coord.rankIndex);
         }
 
-         Coord getCoordFromIndex(int squareIndex)
+        Coord getCoordFromIndex(int squareIndex)
         {
             return Coord(getFileIndex(squareIndex), getRankIndex(squareIndex));
         }
 
-         bool getLightSquare(int fileIndex, int rankIndex)
+        bool getLightSquare(int fileIndex, int rankIndex)
         {
             return (fileIndex + rankIndex) % 2 != 0;
         }
 
-         std::string getSquareNameFromCoordinate(int fileIndex, int rankIndex)
+        std::string getSquareNameFromCoordinate(int fileIndex, int rankIndex)
         {
-            return std::to_string(fileNames[fileIndex]) + std::to_string(rankIndex + 1);
+            return (fileNames[fileIndex] + std::to_string(rankIndex + 1));
         }
 
-         std::string getSquareNameFromIndex(int squareIndex)
+        std::string getSquareNameFromIndex(int squareIndex)
         {
             Coord coor = getCoordFromIndex(squareIndex);
             return getSquareNameFromCoordinate(coor.fileIndex, coor.rankIndex);
--- main.cpp
+++ main.cpp
@@ -3,14 +3,33 @@
 #include "board_representation.h"
 int main()
 {
-    string fenString = "rnbqkbnr/pppp3p/5p2/4p1p1/4P3/5PPB/PPPPN2P/RNBQKR2 w Qkq - 0 1";
+    string fenString = "8/1n3n2/4n3/3n1n2/2n1n3/8/8/8 w - - 0 1";
     ChessBoard chess_board(fenString);
     cout << chess_board << endl;
-    chess_board.drawArray();
+    // chess_board.drawArray();
 
+    // for (int i = 0; i < 64; i++)
+    // {
+    //     cout << i << ' ' << chess_board.grid[Chess::BoardRepresentation::getFileIndex(i)][Chess::BoardRepresentation::getRankIndex(i)] << endl;
+    // }
+
+    Chess::BoardRepresentation::PieceList knights[2] = {Chess::BoardRepresentation::PieceList(10),
+                                                        Chess::BoardRepresentation::PieceList(10)};
     for (int i = 0; i < 64; i++)
     {
-        cout << i << ' ' << Chess::BoardRepresentation::getCoordFromIndex(i) << ' ' << Chess::BoardRepresentation::getFileIndex(i) << " " << Chess::BoardRepresentation::getRankIndex(i) << endl;
+        if (i % 8 == 7)
+        {
+            cout << chess_board.grid[8 - (i / 8) - 1][(i % 8)] << " " << Chess::BoardRepresentation::getSquareNameFromIndex(i) << endl;
+        }
+        else
+        {
+            cout << chess_board.grid[8 - (i / 8) - 1][i % 8] << " " << Chess::BoardRepresentation::getSquareNameFromIndex(i);
+        }
+        if (chess_board.grid[8 - (i / 8) - 1][i % 8] == 'n')
+        {
+            knights[0].addPieceAtSquare(i);
+        }
     }
+    cout << Chess::BoardRepresentation::getSquareNameFromIndex(knights[0][0]) << endl;
     return 0;
 }
