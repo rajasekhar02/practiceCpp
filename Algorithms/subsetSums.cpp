e102aba6a1e1a517407a24c3a62d71814a2f46ff
raja02sekhar08 <raja02sekhar08@gmail.com>
2022-07-22 12:55:33 -0500

    feat: add subsets sum algorithm

---
+++ Algorithms/subsetSums.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> nums;
+
+int countSubsetsSum(int pos, int sum)
+{
+    if (sum == 0)
+    {
+        return 1;
+    }
+    int subsetCount = 0;
+    for (int i = pos; i < nums.size(); i++)
+    {
+        if (sum - nums[i] < 0)
+            continue;
+        int value = countSubsetsSum(i + 1, sum - nums[i]);
+        if (value > 0)
+        {
+            subsetCount += value;
+        }
+    }
+    return subsetCount;
+}
+
+int main()
+{
+    int sum = 15;
+    nums = vector<int>{8, 6, 7, 5, 3, 10, 9};
+    sort(nums.begin(), nums.end());
+    cout << countSubsetsSum(0, sum) << endl;
+    return 0;
+}
