10604a9e8806a39b12286a4c8960c6a8ce0005da
raja02sekhar08 <raja02sekhar08@gmail.com>
2022-08-02 10:41:16 -0500

    feat: add performance calculating template

---
+++ performace_calculating_templace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <ctime>
+using std::clock_t;
+
+int main()
+{
+    double elapsed_secs = 0;
+    clock_t begin = clock();
+    // place the algorithm here to calculate time taken
+    clock_t end = clock();
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    cout << "FIB:\t" << f << "\tTIME:\t" << elapsed_secs << endl;
+    return 0;
+}
