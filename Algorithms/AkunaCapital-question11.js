b6c2a28811f3a9310e3a2e48702c693d4771457c
Raja Sekhar Pothina <rajavishnu123@gmail.com>
2022-09-28 11:40:17 -0500

    Create AkunaCapital-question11.js

---
+++ Algorithms/AkunaCapital-question11.js
@@ -0,0 +1,44 @@
+let str = "aaabaaabaaa" //"aababaabce"  //"aaaaaaaaaa"//"abcdcba" //"aaaaabba" //"aaabaaabaaa"
+let dp = new Array(str.length).fill(1).map(_=>new Array(str.length+1).fill(-1));
+let isPalindrome = function(position,length){
+    if(length == 0 || length == 1){
+        return true;
+    }
+    if(dp[position][length] != -1){
+        return dp
+    }
+    // iterate over possible lengths
+    return str[position] === str[position+length-1] && isPalindrome(position+1, length-2)
+}
+
+for(let everyIndex=0;everyIndex<str.length;everyIndex++){
+     dp[everyIndex][1] = +true;
+     dp[everyIndex][0] = +true;
+}
+
+for(let everyLength=2;everyLength<str.length+1;everyLength++){
+    for(let everyIndex=0;everyIndex<str.length;everyIndex++){
+        dp[everyIndex][everyLength] = +(str[everyIndex] === str[everyIndex+everyLength-1] && dp[everyIndex+1][everyLength-2])
+    }
+}
+
+
+let k = 3;
+
+let noOfNonOverlappingPalins = function(position){
+    if(position>=str.length-1){
+        return 0;
+    }
+    let maxValue = 0;
+    for(let everyIndex1=position; everyIndex1<str.length; everyIndex1++){
+         for(let everyLength=k; everyLength<(str.length-everyIndex1+1); everyLength++){
+            let value = dp[everyIndex1][everyLength] + noOfNonOverlappingPalins(everyIndex1+everyLength);
+            if(value>maxValue){
+                maxValue=value;
+            }
+        }
+    }
+    return maxValue;
+}
+// console.log(dp)
+console.log(noOfNonOverlappingPalins(0))
